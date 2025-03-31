85da0343a693cd773505f71881cda7ca8c0e936c
gtvb <gabriel.tiso@proton.me>
2025-03-31 08:51:35 -0300

    feat: adicionar melhorias da Tarefa 4

--- js/componente.js
+++ js/componente.js
@@ -1,3 +1,39 @@
+const data = [
+  {
+    id: 1,
+    disciplina: "S05 - Interface Homem-máquina",
+    data: "ter",
+    horario: "10:00",
+    local: "P1-S17",
+    prova_alert: false,
+    prova: "12/05",
+    frequencia: "10/25",
+    nota: "9"
+  },
+  {
+    id: 2,
+    disciplina: "E01 - Circuitos Elétricos em Corrente Contínua",
+    data: "ter",
+    horario: "10:00",
+    local: "P1-S17",
+    prova_alert: true,
+    prova: "12/05",
+    frequencia: "10/25",
+    nota: "5"
+  },
+  {
+    id: 3,
+    disciplina: "M02 - Álgebra e Geometria Analítica",
+    data: "ter",
+    horario: "10:00",
+    local: "P1-S17",
+    prova_alert: true,
+    prova: "12/05",
+    frequencia: "10/25",
+    nota: "7"
+  }
+];
+
 class AulasComponent extends HTMLElement {
     constructor() {
       super();
@@ -11,8 +47,13 @@ class AulasComponent extends HTMLElement {
   
     async loadData() {
       try {
-        const response = await fetch('https://github.com/gabrieltiso-inatel/tree/main/assets/aulas.json');
-        const aulas = await response.json();
+        const fakePromise = new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(data);
+          }, 100);
+        })
+
+        const aulas = await fakePromise;
         this.render(aulas);
       } catch (error) {
         console.error('Erro ao carregar os dados das aulas:', error);
@@ -25,20 +66,31 @@ class AulasComponent extends HTMLElement {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = '../css/componente_aulas.css'; 
+
       this.shadowRoot.appendChild(link); 
-  
       this.shadowRoot.innerHTML += `
         <div>
           ${aulasDia.map(a => {
             let provaDisplay = a.prova_alert ? '' : 'display: none;';
+            let nota = parseInt(a.nota);
+
+            let provaNota;
+            if (nota < 6) {
+              provaNota = 'low';
+            } else if (nota >= 6 && nota < 8) {
+              provaNota = 'medium';
+            } else {
+              provaNota = 'high';
+            }
+
             return `
               <div class="comp-aula">
-                <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
-                <div class="titulo_aula">${a.disciplina}</div>
-                <p class="p">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
+                <div class="lable-prova p_lable arimo-bold" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
+                <div class="titulo_aula arimo-bold">${a.disciplina}</div>
+                <p class="p arimo-normal">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
                 <div class="lables">
-                  <div class="lable-frequencia p_lable">FALTAS: <b>${a.frequencia}</b></div>
-                  <div class="lable-nota p_lable">CR: <b>${a.nota}</b></div>
+                  <div class="lable-frequencia p_lable arimo-bold">FALTAS: <b>${a.frequencia}</b></div>
+                  <div class="lable-nota p_lable arimo-bold" data-grade=${provaNota}>CR: <b>${a.nota}</b></div>
                 </div>
               </div>
             `;
